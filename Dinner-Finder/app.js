6a13ca7fd4c31c437b62e40f9c447abe9f4eeaf5
Aarik Robles <roblesaarik@gmail.com>
2020-02-02 12:55:48 -0800

    Created Modals

--- Dinner-Finder/app.js
+++ Dinner-Finder/app.js
@@ -7,11 +7,12 @@ $(() => {
     }).then(
       data => {
         console.log(data);
-        let $h3 = $("<h3>").text(data.meals["0"].strMeal);
+        let $h3 = $("<h3>")
+          .text(data.meals["0"].strMeal)
+          .attr("id", data.meals[`0`].idMeal);
         let $img = $(
           `<img src="${data.meals["0"].strMealThumb}" style='width: 80%;'/>`
-        );
-        let $p = $("<p>").text(data.meals["0"].strInstructions);
+        ).attr("id", data.meals[`0`].idMeal);
         $(place).append($h3);
         $(place).append($img);
       },
@@ -24,11 +25,33 @@ $(() => {
   randomDish(".random1");
   randomDish(".random2");
 
-  // Search Bar event listener
-  $("form").on("submit", () => {
+  // hides the Current list of recipes
+  const hideCurrentList = () => {
     if ($("li").text().length > 0) {
       $("li").hide();
+      $(".listPic").hide();
     }
+  };
+
+  // Creates list of recipes
+  const createList = data => {
+    for (let i = 0; i <= data.meals.length; i += 1) {
+      let $li = $("<li>")
+        .text(data.meals[`${i}`].strMeal)
+        .attr("id", data.meals[`${i}`].idMeal);
+      let $img = $(
+        `<img src="${data.meals[`${i}`].strMealThumb}" style='width: 40%;'/>`
+      )
+        .addClass("listPic")
+        .attr("id", data.meals[`${i}`].idMeal);
+      $("ul").append($li);
+      $("ul").append($img);
+    }
+  };
+
+  // Search Bar event listener
+  $("form").on("submit", () => {
+    hideCurrentList();
     $("#featured").hide();
     event.preventDefault();
 
@@ -39,17 +62,7 @@ $(() => {
     }).then(
       data => {
         console.log(data);
-
-        for (let i = 0; i <= data.meals.length; i += 1) {
-          let $li = $("<li>").text(data.meals[`${i}`].strMeal);
-          let $img = $(
-            `<img src="${
-              data.meals[`${i}`].strMealThumb
-            }" style='width: 40%;'/>`
-          );
-          $("ul").append($li);
-          $("ul").append($img);
-        }
+        createList(data);
       },
 
       () => {
@@ -57,4 +70,54 @@ $(() => {
       }
     );
   });
+
+  // Click event listener for country buttons
+  $(".country").on("click", () => {
+    hideCurrentList();
+    $("#featured").hide();
+    event.preventDefault();
+    $.ajax({
+      url:
+        "https://www.themealdb.com/api/json/v1/1/filter.php?a=" +
+        $(event.target).text(),
+    }).then(
+      data => {
+        console.log(data);
+        createList(data);
+      },
+
+      () => {
+        console.log("bad");
+      }
+    );
+  });
+
+  // Create modal when recipe is clicked
+  const $modal = $("#modal");
+  const $closeBtn = $("#close");
+  const openModal = () => {
+    $modal.css("display", "block");
+  };
+  $("ul").on("click", () => {
+    openModal();
+
+    $.ajax({
+      url:
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" +
+        $(event.target).attr("id"),
+    }).then(
+      data => {
+        console.log(data);
+      },
+      () => {
+        console.log("bad");
+      }
+    );
+  });
+
+  // close modal
+  const closeModal = () => {
+    $modal.css("display", "none");
+  };
+  $closeBtn.on("click", closeModal);
 });
